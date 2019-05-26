3571bc5fe51c964dbe18ee5a4ca60e94171d562e
Rosvin Noguera Chavarria <rnoguera415@hotmail.com>
2019-05-26 00:24:03 -0600

    Nuevo Cambio 3

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 class Main{
 
     public static void main(String[] args){
-        System.out.println("Hola mundo desde Java");
         System.out.println("La conexion con Github se ha hecho");
-        System.out.println("Ya casi Ya casi");
     }
 }
